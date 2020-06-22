9df1b229feef04cb95c23f02b45d118aa7c5e5d9
HodaraMachado <hodaramachado@gmail.com>
2020-06-22 01:15:31 -0300

    Primeiro commit

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        // 1. Crie um programa que imprima na tela a sua idade e a data de nascimento utilizando apenas variáveis do tipo inteiro. (formato de saída da data dd/mm/aaaa)
+
+    int idade = 20;
+    int dia = 06;
+    int mes = 11;
+    int ano = 1997;
+
+        System.out.println("Minha idade é: "+ idade);
+        System.out.println("Minha data de nascimento é: "+ dia +"/" + mes +"/" + ano);
+
+
+    }
+}
